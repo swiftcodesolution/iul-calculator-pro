5aea5cb631f9619781fece81529f62d898fd99a7
ALI <ali-dev@texastechdemo.com>
2025-06-02 19:04:58 +0500

    prepare for prisma deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["iul-calculator-pro-nu.vercel.app/", "localhost:3000"], // Add your production domain
+    },
+  },
 };
 
 export default nextConfig;
