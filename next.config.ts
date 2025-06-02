68481974372a9f6a79e5372f6c1d99a8811796ed
ALI <ali-dev@texastechdemo.com>
2025-06-02 19:33:47 +0500

    disabled linting rules

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
       allowedOrigins: ["iul-calculator-pro-nu.vercel.app/", "localhost:3000"], // Add your production domain
     },
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Skips ESLint during Vercel build
+  },
 };
 
 export default nextConfig;
