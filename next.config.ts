9b04dae0d82832d4111902a889104eda0f601129
ALI <ali-dev@texastechdemo.com>
2025-06-02 22:43:49 +0500

    ...

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["iul-calculator-pro-nu.vercel.app/", "localhost:3000"], // Add your production domain
-    },
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // Skips ESLint during Vercel build
-  },
 };
 
 export default nextConfig;
