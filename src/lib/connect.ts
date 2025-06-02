4b2844e9db109829cbe3556dea1a126b124bde48
ALI <ali-dev@texastechdemo.com>
2025-06-02 22:50:26 +0500

    ...

--- src/lib/connect.ts
+++ src/lib/connect.ts
@@ -1,16 +1,13 @@
+// lib/connect.ts
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
-declare const globalThis: {
-  prisma: ReturnType<typeof prismaClientSingleton>;
-  prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = global.prisma ?? new PrismaClient();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
