d31801d4839fd14b3a5f696bcfb730d54b8b674a
ALI <ali-dev@texastechdemo.com>
2025-06-12 22:34:28 +0500

    vercel speed insights integrated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import "./globals.css";
 
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import SessionProviderWrapper from "@/components/auth/SessionProviderWrapper";
 import { FileProvider } from "@/context/FileContext";
 
