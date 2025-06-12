dae4cae8a3caab9ea0063528bc14075f498644e7
ALI <ali-dev@texastechdemo.com>
2025-06-13 02:06:55 +0500

    inflation calculator changes

--- src/components/InflationCalculator.tsx
+++ src/components/InflationCalculator.tsx
@@ -57,72 +57,63 @@ export default function InflationCalculator() {
   };
 
   return (
-    <div className="w-full mx-auto p-4 space-y-5">
+    <div className="w-full space-y-5">
       <h1 className="text-xl font-semibold text-center">
         Inflation Calculator
       </h1>
       <Card className="border p-4">
-        <CardContent className="space-y-6 pt-0">
-          <div className="flex items-center gap-4">
-            <Label className="w-1/2 text-right">
-              Current Monthly Expenses $
+        <CardContent className="space-y-6">
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-4">
+              <Label className="w-1/2 text-right">
+                Current Monthly Expenses $
+              </Label>
+              <Input
+                className="w-1/2"
+                type="number"
+                value={currentExpenses}
+                onChange={(e) => handleCurrentExpensesChange(e.target.value)}
+                placeholder="Enter amount"
+                min="0"
+              />
+            </div>
+            <div className="flex items-center gap-4">
+              <Label className="w-1/2 text-right">Target Age</Label>
+              <Input
+                className="w-1/2"
+                type="number"
+                value={targetAge}
+                onChange={(e) => handleAgeChange(e.target.value, setTargetAge)}
+                placeholder="Enter age"
+                min="0"
+              />
+            </div>
+            <div className="flex items-center gap-4">
+              <Label className="w-1/2 text-right">Inflation Rate (%)</Label>
+              <Input
+                className="w-1/2"
+                type="number"
+                value={inflationRate}
+                onChange={(e) => handleInflationChange(e.target.value)}
+                placeholder="Enter rate"
+                min="0"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <Label className="text-center text-2xl font-bold">
+              Expenses at Age {targetAge} - Inflation rate {inflationRate}%
             </Label>
-            <Input
-              className="w-1/2"
-              type="number"
-              value={currentExpenses}
-              onChange={(e) => handleCurrentExpensesChange(e.target.value)}
-              placeholder="Enter amount"
-              min="0"
-            />
-          </div>
-          <div className="flex items-center gap-4">
-            <Label className="w-1/2 text-right">Current Age</Label>
-            <Label className="w-1/2">{currentAge}</Label>
-          </div>
-          <div className="flex items-center gap-4">
-            <Label className="w-1/2 text-right">Target Age</Label>
-            <Input
-              className="w-1/2"
-              type="number"
-              value={targetAge}
-              onChange={(e) => handleAgeChange(e.target.value, setTargetAge)}
-              placeholder="Enter age"
-              min="0"
-            />
-          </div>
-          <div className="flex items-center gap-4">
-            <Label className="w-1/2 text-right">Inflation Rate (%)</Label>
-            <Input
-              className="w-1/2"
-              type="number"
-              value={inflationRate}
-              onChange={(e) => handleInflationChange(e.target.value)}
-              placeholder="Enter rate"
-              min="0"
-            />
+            {!futureExpenses ? (
+              <p className="text-center text-2xl font-bold text-gray-400">
+                Calculated Amount
+              </p>
+            ) : (
+              <p className="text-center text-4xl font-bold">{futureExpenses}</p>
+            )}
           </div>
         </CardContent>
       </Card>
-      <div className="flex flex-col items-center gap-4">
-        <Label className="text-center text-4xl font-bold">
-          Expenses at Age {targetAge} - Inflation rate {inflationRate}%
-        </Label>
-        {!futureExpenses ? (
-          <p className="text-center text-4xl font-bold text-gray-400">
-            Calculated Amount
-          </p>
-        ) : (
-          <p className="text-center text-4xl font-bold">{futureExpenses}</p>
-        )}
-        {/* <Input
-          className="w-1/2 text-4xl font-bold"
-          type="number"
-          value={futureExpenses}
-          readOnly
-          placeholder="Calculated amount"
-        /> */}
-      </div>
     </div>
   );
 }
