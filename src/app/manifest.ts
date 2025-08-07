ee8b72f0c0efcdee2bc84989f5facaa079a3c827
swift code solution <dev@swiftcodesolution.com>
2025-08-08 04:46:26 +0500

    pwa icon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,13 +11,13 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#ffffff",
     icons: [
       {
-        src: "/icons/icon-192x192.png",
+        src: "/icon.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icons/icon-512x512.png",
+        src: "/icon.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
