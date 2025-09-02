086768271f0da122d2c779cd2bdd569681012516
swift code solution <dev@swiftcodesolution.com>
2025-09-03 02:33:37 +0500

    removed extra pages

--- src/app/dashboard/downloads-content/page.tsx
+++ src/app/dashboard/downloads-content/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 export const dynamic = "force-dynamic";
 
+import { Suspense } from "react";
 import DownloadContentPageInner from "@/components/inners/DownloadsContentInner";
 
 export default function DownloadContentPage() {
-  return <DownloadContentPageInner />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DownloadContentPageInner />
+    </Suspense>
+  );
 }
