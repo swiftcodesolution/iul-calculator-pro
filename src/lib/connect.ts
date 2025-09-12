4ed1f2a43297945cc8eb93a22c0af13a7dbc65aa
swift code solution <dev@swiftcodesolution.com>
2025-09-12 21:33:50 +0500

    db logs

--- src/lib/connect.ts
+++ src/lib/connect.ts
@@ -1,12 +1,40 @@
-// lib/connect.ts
+// // lib/connect.ts
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var prisma: PrismaClient | undefined;
+// }
+
+// const prisma = global.prisma ?? new PrismaClient();
+
+// export default prisma;
+
+// if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
 const prisma = global.prisma ?? new PrismaClient();
+
+prisma.$use(async (params, next) => {
+  // Execute the query first
+  const result = await next(params);
+
+  // Log the database action
+  await prisma.databaseLog.create({
+    data: {
+      model: params.model,
+      action: params.action,
+      args: params.args ? JSON.stringify(params.args) : null,
+    },
+  });
+
+  return result;
+});
 
 export default prisma;
 
